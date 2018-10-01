7c701866faa7c9bb769b5a83d62ce2e31e45c79f
Daniel Tejeda <juandanieltd@gmail.com>
2018-09-30 21:44:00 -0600

    Prints silenciados

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,19 +3,11 @@
 
 class User_model extends CI_Model {
 
-    //Desde modelo también podemos acceder a la base de datos.
+    
     public function create($user){
-        
         $user_to_array = (array) $user;
-        echo var_dump($user_to_array);
-
         $statement = $this->db->conn_id->prepare("INSERT INTO user VALUES(:uuid, :name, :last_name, :street, :second_street, :street_number, :zip_code, :phone_number, :email, :password, :status, :country)");
-
         $statement->execute($user_to_array);
-
-        echo "<br><br><br>";
-        print_r($statement->errorInfo());
-
     }
 
 
